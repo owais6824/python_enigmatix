a1b25df3ca96a253b33ea41e07eb977cc48dcf95
Muhammad Awais <137401321+owais6824@users.noreply.github.com>
2024-02-28 16:35:31 +0500

    Add files via upload
    basic concept to get started with boolean...

---
+++ 3_module2_boolean.py
@@ -0,0 +1,25 @@
+"""
+    Welcome...!
+    Module # 2 (Boolean)
+"""
+
+print(100 > 90)
+print(2 == 3)
+print(90 < 80)
+
+# checking variables
+a = 23
+b = 30
+if b > a:
+    print("First number is greater then Second number")
+else:
+    print("Second number is greater then First number")
+
+# evaluate the variables
+print(bool(123))
+print(bool("hello"))
+print(bool(23.67))
+print(bool(0))
+print(bool([]))
+print(bool({}))
+print(bool(()))
