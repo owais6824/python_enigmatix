8f0fc3c05a20dcc22ba2190c93597262393468bb
Muhammad Awais <137401321+owais6824@users.noreply.github.com>
2024-04-29 16:40:03 +0500

    Add files via upload
     You are given a positive integer. 
     You can only use one for loop and one print statement. 
     Print a numerical triangle of height like the one below:

---
+++ Test Questions/3_module_8_test3.py
@@ -0,0 +1,14 @@
+'''
+    Welcome...!
+    Module # 8 (Test 3)
+    You are given a positive integer. 
+    You can only use one for loop and one print statement. 
+    Print a numerical triangle of height like the one below:
+
+'''
+
+num = 5
+for i in range(1, num):
+    print(str(i)*i, "\n")
+
+
